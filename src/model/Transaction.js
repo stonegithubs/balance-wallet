4fd559697bd12c1b234cb530c65e6db4af840738
jinchung <jin.chung17@gmail.com>
2019-01-25 15:06:57 -0500

    fixes for watermelon

--- src/model/Transaction.js
+++ src/model/Transaction.js
@@ -1,9 +1,10 @@
 import { Model } from '@nozbe/watermelondb';
+import { field } from '@nozbe/watermelondb/decorators'
 
 export default class Transaction extends Model {
   static table = 'transactions'
+
+  @field('hash') hash
+  @field('timestamp') timestamp
 }
 
-// TODO snake case, camelcase
-@field('hash') hash
-@field('timestamp') timestamp
